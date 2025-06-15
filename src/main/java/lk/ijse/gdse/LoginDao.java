ffe9621047fd68245f21bcee8e5593588a29d64e
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-06-15 09:06:32 +0530

    create the login Servlet

---
+++ src/main/java/lk/ijse/gdse/LoginDao.java
@@ -0,0 +1,27 @@
+package lk.ijse.gdse;
+
+import java.sql.*;
+
+public class LoginDao {
+    public String checkLogin(String username, String password){
+        String url = "jdbc:mysql://localhost:3306/cms";
+        String user = "root";
+        String pass = "1234";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            Connection connection = DriverManager.getConnection(url,user,pass);
+            String sql = "select * from user where uname=? and upassword=?";
+            PreparedStatement preparedStatement = connection.prepareStatement(sql);
+            preparedStatement.setString(1,username);
+            preparedStatement.setString(2,password);
+            ResultSet resultSet = preparedStatement.executeQuery();
+            if(resultSet.next()){
+                return resultSet.getString("urole");
+            }
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+}
--- src/main/java/lk/ijse/gdse/LoginServlet.java
+++ src/main/java/lk/ijse/gdse/LoginServlet.java
@@ -1,5 +1,38 @@
 package lk.ijse.gdse;
 
-public class LoginServlet {
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
+import java.io.IOException;
+
+@WebServlet("/loginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String username = req.getParameter("username");
+        String password = req.getParameter("password");
+
+        LoginDao loginDao = new LoginDao();
+        String role = String.valueOf(loginDao.checkLogin(username,password));
+
+        if (role != null) {
+            if (role.equals("admin")) {
+                resp.sendRedirect("adminDashBord.jsp");
+            } else if (role.equals("user")) {
+                resp.sendRedirect("userDashBoard.jsp");
+            } else {
+                System.out.println("Invalid username or password");
+                resp.sendRedirect("login.jsp");
+            }
+        } else {
+            resp.sendRedirect("login.jsp");
+        }
+
+        System.out.println("Username: " + username);
+        System.out.println("Password: " + password);
+        System.out.println("User role: " + role);
+    }
 }
