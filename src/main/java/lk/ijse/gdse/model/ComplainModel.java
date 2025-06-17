55cdd3105802bc8f963fc5142bc4f36e1c840fdf
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-06-17 11:37:09 +0530

    update ComplainModel

--- src/main/java/lk/ijse/gdse/model/ComplainModel.java
+++ src/main/java/lk/ijse/gdse/model/ComplainModel.java
@@ -14,20 +14,21 @@ public class ComplainModel {
     String user = "root";
     String pass = "1234";
 
-    public boolean saveComplaint(String complaintId , String userName, String subject, String date, String description) {
+    public boolean saveComplaint(String complaintId , String userName, String subject, String date, String description,String status) {
 
 
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, user, pass);
 
-            String sql = "INSERT INTO complaint (cid, uname, subject, description, date) VALUES (?, ?, ?, ?, ?)";
+            String sql = "INSERT INTO complaint (cid, uname, subject, description, date,status) VALUES (?, ?, ?, ?, ?, ?)";
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setString(1, complaintId); // now passed from servlet
             stmt.setString(2, userName);
             stmt.setString(3, subject);
             stmt.setString(4, description);
             stmt.setString(5, date);
+            stmt.setString(6, "pending");
 
             int result = stmt.executeUpdate();
             return result > 0;
@@ -56,7 +57,9 @@ public class ComplainModel {
                         rs.getString("uname"),
                         rs.getString("subject"),
                         rs.getString("description"),
-                        rs.getString("date")
+                        rs.getString("date"),
+                        rs.getString("status")
+
                 ));
             }
 
@@ -66,7 +69,7 @@ public class ComplainModel {
         return complaints;
     }
 
-    public List<ComplainDto> getAllComplaints(String uname, String subject, String description, String userName) {
+    public List<ComplainDto> getAllComplaints(String uname, String subject, String description, String date , String status) {
         List<ComplainDto> complaints = new ArrayList<>();
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
@@ -81,7 +84,8 @@ public class ComplainModel {
                         rs.getString("uname"),
                         rs.getString("subject"),
                         rs.getString("description"),
-                        rs.getString("date")
+                        rs.getString("date"),
+                        rs.getString("status")
                 ));
             }
 
@@ -91,18 +95,19 @@ public class ComplainModel {
         return complaints;
     }
 
-    public boolean updateComplaint(String complaintId, String userName, String subject, String date, String description) {
+    public boolean updateComplaint(String complaintId, String userName, String subject, String date, String description, String status) {
         try {
             Class.forName("com.mysql.cj.jdbc.Driver");
             Connection connection = DriverManager.getConnection(url, user, pass);
 
-            String sql = "UPDATE complaint SET uname = ?, subject = ?, description = ?, date = ? WHERE cid = ?";
+            String sql = "UPDATE complaint SET uname = ?, subject = ?, description = ?, date = ?, status = ? WHERE cid = ?";
             PreparedStatement stmt = connection.prepareStatement(sql);
             stmt.setString(1, userName);
             stmt.setString(2, subject);
             stmt.setString(3, description);
             stmt.setString(4, date);
-            stmt.setString(5, complaintId);
+            stmt.setString(5, status);
+            stmt.setString(6, complaintId);
 
             int result = stmt.executeUpdate();
             return result > 0;
