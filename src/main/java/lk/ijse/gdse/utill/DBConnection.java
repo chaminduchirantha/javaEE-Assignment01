77a8c7e9582c94e3a8d28da75b2ae4530dd7c7a7
chaminduchirantha <chaminduchirantha73@gmail.com>
2025-06-17 20:00:49 +0530

    create the DBConnection class

---
+++ src/main/java/lk/ijse/gdse/utill/DBConnection.java
@@ -0,0 +1,22 @@
+package lk.ijse.gdse.utill;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnection {
+    private static final String URL = "jdbc:mysql://localhost:3306/cms";
+    private static final String USER = "root";
+    private static final String PASS = "1234";
+
+    static {
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Failed to load JDBC Driver", e);
+        }
+    }
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(URL, USER, PASS);
+    }
+}
